445b200d088efc189ce519f47006077c1a9f32f8
IEROA7 <145680665+IEROA7@users.noreply.github.com>
2025-01-17 10:26:11 +0300

    Kütüphane Yönetim Sistemi dosyaları eklendi

---
+++ main.py
@@ -0,0 +1,13 @@
+import setup
+import tkinter as tk
+from gui import LibraryApp
+import threading
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client, books, users, date_setting = setup.run_setup()
+    thread = threading.Thread(target=setup.background_checker, args=(users, books), daemon=True)
+    thread.start()
+    root = tk.Tk()
+    app = LibraryApp(books, users, root)  # Parametrelerle sınıfı başlat
+    root.mainloop()
